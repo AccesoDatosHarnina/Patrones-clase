a702e80c00c426c6282e9820731737b871e8499f
programacion <>
2022-12-19 13:47:53 +0100

    Observer v2

--- Examen Patrones Solucion/src/modelo/entidades/Hormiguero.java
+++ Examen Patrones Solucion/src/modelo/entidades/Hormiguero.java
@@ -1,5 +1,7 @@
 package modelo.entidades;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -7,66 +9,65 @@ import java.util.List;
 import modelo.insectos.*;
 import modelo.soporte.*;
 
-public class Hormiguero {
-    Statistics statistics;
-    public final int cantidadHormigasTotal = 30;
-    public final int cantidadHormigasGuerreras = 15;
+public class Hormiguero implements PropertyChangeListener {
+	Statistics statistics;
+	public final int cantidadHormigasTotal = 30;
+	public final int cantidadHormigasGuerreras = 15;
 
-    private boolean atacada = false;
-    private boolean tareaAcabada = false;
+	private boolean atacada = false;
+	private boolean tareaAcabada = false;
 
+	private List<Hormiga> hormigas;
+	private static long id = 1;
 
-    private List<Hormiga> hormigas;
-    private static long id = 1;
+	public Hormiguero() {
+		super();
+		hormigas = new ArrayList<Hormiga>();
+		statistics = new Statistics();
+	}
 
-    public Hormiguero() {
-        super();
-        hormigas = new ArrayList<Hormiga>();
-        statistics = new Statistics();
-    }
-
-    public void funciona() {
-        int historia = 0;
-        int guerreras = 0;
-        do {
-            historia++;
-            //las hormigas hacen lo que le toca
-            for (int i = 0; i < hormigas.size(); i++) {
-                Hormiga hormiga = hormigas.get(i);
-                hormiga.hacerTarea();
-            }
-            if (historia % 100 == 0) {
-                //estamos en guerra o en paz
-                atacada = !atacada;
-                if (atacada) {
-                    System.out.println("estamos en guerra");
-                    convertirHormigasGuerra(cantidadHormigasGuerreras - contarHormigasGuerreras());
-                } else {
-                    //fin de la guerra
-                    System.out.println("volvemos a la paz");
-                    convertirHormigasPaz();
-                }
-            }
-            //puede que se hayan muerto algunas
-            if (atacada) {
-                guerreras = cantidadHormigasGuerreras - contarHormigasGuerreras();
+	public void funciona() {
+		int historia = 0;
+		int guerreras = 0;
+		do {
+			historia++;
+			// las hormigas hacen lo que le toca
+			for (int i = 0; i < hormigas.size(); i++) {
+				Hormiga hormiga = hormigas.get(i);
+				hormiga.hacerTarea();
+			}
+			if (historia % 100 == 0) {
+				// estamos en guerra o en paz
+				atacada = !atacada;
+				if (atacada) {
+					System.out.println("estamos en guerra");
+					convertirHormigasGuerra(cantidadHormigasGuerreras - contarHormigasGuerreras());
+				} else {
+					// fin de la guerra
+					System.out.println("volvemos a la paz");
+					convertirHormigasPaz();
+				}
+			}
+			// puede que se hayan muerto algunas
+			if (atacada) {
+				guerreras = cantidadHormigasGuerreras - contarHormigasGuerreras();
 //                if (guerreras > 0) {
 //                    System.out.println("hormigas guerreras muertas " + guerreras);
 //                }
-            } else {
-                guerreras = 0;
-            }
-            enterrarHormigas();
-            crearHormigas(guerreras);
-            if (historia == 310) tareaAcabada = true;
+			} else {
+				guerreras = 0;
+			}
+			crearHormigas(guerreras);
+			if (historia == 310)
+				tareaAcabada = true;
 //            if (cantidad % 20 == 0) System.out.println("historia " + cantidad);
-        } while (!isTareaAcabada());
-        //mostrar estadisticas
+		} while (!isTareaAcabada());
+		// mostrar estadisticas
 //        System.out.println(statistics.getCurrentMediaAlimento());
 //        System.out.println(statistics.getCurrentIndiceGlobal());
-    }
+	}
 
-    public List<Hormiga> getHormigas() {
+	public List<Hormiga> getHormigas() {
 		return hormigas;
 	}
 
@@ -75,63 +76,60 @@ public class Hormiguero {
 	}
 
 	private void convertirHormigasGuerra(int i) {
-        int contador=0;
-        for (int j = 0; j < cantidadHormigasGuerreras; j++) {
-            hormigas.get(j).setGuerrera(true);
-            contador++;
-        }
-        System.out.println("hormigas  guerreras convertidas "+contador);
-    }
+		int contador = 0;
+		for (int j = 0; j < cantidadHormigasGuerreras; j++) {
+			hormigas.get(j).setGuerrera(true);
+			contador++;
+		}
+		System.out.println("hormigas  guerreras convertidas " + contador);
+	}
 
-    private void convertirHormigasPaz() {
-        int contador=0;
-        for (Hormiga hormiga : hormigas) {
-            if (hormiga.isGuerrera()) {
-                hormiga.setGuerrera(false);
-                contador++;
-            }
-        }
-        System.out.println("hormigas  convertidas a la paz "+contador);
-    }
+	private void convertirHormigasPaz() {
+		int contador = 0;
+		for (Hormiga hormiga : hormigas) {
+			if (hormiga.isGuerrera()) {
+				hormiga.setGuerrera(false);
+				contador++;
+			}
+		}
+		System.out.println("hormigas  convertidas a la paz " + contador);
+	}
 
-    private int contarHormigasGuerreras() {
-        int contador = 0;
-        for (Hormiga hormiga : hormigas) {
-            if (hormiga.isGuerrera()) contador++;
-        }
-        return contador;
-    }
+	private int contarHormigasGuerreras() {
+		int contador = 0;
+		for (Hormiga hormiga : hormigas) {
+			if (hormiga.isGuerrera())
+				contador++;
+		}
+		return contador;
+	}
 
-    private void enterrarHormigas() {
-        for (Iterator<Hormiga> iterator = hormigas.iterator(); iterator.hasNext(); ) {
-            Hormiga next = iterator.next();
-            if (!next.isAlive()) {
-                statistics.addData(HormigaDataAdapter.convert(next));
-                iterator.remove();
-            }
-        }
-    }
+	private void crearHormigas(int guerreras) {
+		int contador = 0;
+		for (int i = hormigas.size(); i < cantidadHormigasTotal; i++) {
+			Hormiga hormiga = new Hormiga(id++,this);
+			if (guerreras-- > 0) {
+				hormiga.setGuerrera(true);
+			}
+			hormigas.add(hormiga);
+			contador++;
+		}
+	}
 
-    private void crearHormigas(int guerreras) {
-        int contador = 0;
-        for (int i = hormigas.size(); i < cantidadHormigasTotal; i++) {
-            Hormiga hormiga = new Hormiga(id++);
-            if (guerreras-- > 0) {
-                hormiga.setGuerrera(true);
-            }
-            hormigas.add(hormiga);
-            contador++;
-        }
-    }
+	private boolean isTareaAcabada() {
+		return tareaAcabada;
+	}
 
+	private void setTareaAcabada(boolean tareaAcabada) {
+		this.tareaAcabada = tareaAcabada;
+	}
 
-    private boolean isTareaAcabada() {
-        return tareaAcabada;
-    }
-
-    private void setTareaAcabada(boolean tareaAcabada) {
-        this.tareaAcabada = tareaAcabada;
-    }
-
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		PropertyChangeEvent evt2 = evt;
+		Hormiga hormiga = (Hormiga) evt.getNewValue();
+		statistics.addData(HormigaDataAdapter.convert(hormiga));
+		hormigas.remove(hormiga);
+	}
 
 }
--- Examen Patrones Solucion/src/modelo/insectos/Hormiga.java
+++ Examen Patrones Solucion/src/modelo/insectos/Hormiga.java
@@ -1,28 +1,32 @@
 package modelo.insectos;
 
+import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import modelo.entidades.Hormiguero;
 import modelo.soporte.Alimento;
 
 public class Hormiga {
-
-
 	List<Alimento> alimentos;
 	List<Integer> luchas;
-	private final int maxima=50;
-	private int vida = new Random().nextInt(maxima)+1;
+	private final int maxima = 50;
+	private int vida = new Random().nextInt(maxima) + 1;
 	private int edad = 0;
 	public long id;
 	protected int incrementoVidaPorDefecto = 1;
-	private boolean guerrera=false;
+	private boolean guerrera = false;
+	// observer
+	PropertyChangeSupport pcs;
 
-	public Hormiga(long id) {
+	public Hormiga(long id,Hormiguero hormiguero) {
 		super();
 		this.id = id;
 		alimentos = new ArrayList();
-		luchas=new ArrayList<>();
+		luchas = new ArrayList<>();
+		pcs = new PropertyChangeSupport(this);
+		pcs.addPropertyChangeListener(hormiguero);
 	}
 
 	public boolean isGuerrera() {
@@ -58,23 +62,19 @@ public class Hormiga {
 	}
 
 	public void hacerTarea() {
-		if(this.isAlive()) {
-			try {
-				Thread.sleep(1);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+		if (this.isAlive()) {
 			if (guerrera) {
-				incrementaEdad(vida/4);
-				int tiempoMaximoLucha=100;
+				incrementaEdad(vida / 4);
+				int tiempoMaximoLucha = 100;
 				luchas.add(new Random().nextInt(tiempoMaximoLucha));
-			}else{
+			} else {
 				incrementaEdad(incrementoVidaPorDefecto);
 				alimentos.add(Alimento.getRandomAlimento());
 			}
-		}else{
-//			System.out.println(id+" mori por mi hormiguero");
+		} else {
+			pcs.firePropertyChange("muerte", null, this);
 		}
+
 	};
 
 	public boolean isAlive() {
